55e1b347810eddf5268273d9c20baf27f57b248f
AlexanderSprns <GITHUB alex.sprns@gmail,com>
2020-06-17 14:59:57 +0300

    added solution steps

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
         input();
     }
-
+    //TODO add new call after end
     private static void input() {
-        double a = 0, b = 0, c = 0;
+        double a, b, c;
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.print("Введите коэффициенты уравнения. \na = ");
+            System.out.print("Введите коэффициенты уравнения: \na = ");
             a = scanner.nextDouble();
 
             while (a == 0) {
@@ -37,21 +37,27 @@ public class Main {
     private static void calculateRoots(double a, double b, double c) {
         double D, rootX1, rootX2;
         D = b * b - 4 * a * c;
+        System.out.println("D = b^2 - 4ac = " + D);
         // TODO add steps of solution
         if (D > 0) {
             rootX1 = (-b + Math.sqrt(D)) / (2 * a);
             rootX2 = (-b - Math.sqrt(D)) / (2 * a);
-            System.out.println("Root X1 = " + rootX1);
-            System.out.println("Root X2 = " + rootX2);
+            System.out.println("x1 = (-b + \u221AD) / 2a");
+            System.out.println("x1 = (-b - \u221AD) / 2a");
+            System.out.println("Корень x1 = " + rootX1);
+            System.out.println("Корень x2 = " + rootX2);
         } else if (D == 0) {
             rootX1 = rootX2 = -b / 2 * a;
-            System.out.println("Root X1 = Root X2 = " + rootX1);
-        } else if (D < 0) {                                                 // complex roots
+            System.out.println("x1 = x2 = -b / 2a");
+            System.out.println("Корень x1, x2 = " + rootX1);
+        } else if (D < 0) {
             double realNumber, imaginaryUnit;
             realNumber = -b / (2 * a);
             imaginaryUnit = Math.sqrt(Math.abs(D)) / (2 * a);
-            System.out.println("Root X1 = " + realNumber + " + i" + imaginaryUnit);
-            System.out.println("Root X2 = " + realNumber + " - i" + imaginaryUnit);
+            System.out.println("x1 = (-b + \u221A|D|) / 2a");
+            System.out.println("x1 = (-b - \u221A|D|) / 2a");
+            System.out.println("Корень x1 = " + realNumber + " + i" + imaginaryUnit);
+            System.out.println("Корень x2 = " + realNumber + " - i" + imaginaryUnit);
         }
     }
 }
